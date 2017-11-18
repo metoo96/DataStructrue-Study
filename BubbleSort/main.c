e403a0fe998fb1e05532888748bf6c4543a6f5b8
zhuguozhu123 <zgz1211651004>
2017-11-18 23:19:54 +0800

    second commit

---
+++ BubbleSort/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*内容 冒泡排序
+*作者 Guozhu Zhu
+*时间 2017/11/18
+*/
+int main()
+{
+    int arr[] = {1, 2, 5, 3, 2, 7, 1, 4, 7, 8, 5, 4, 10 , 22, 44, 88};
+    int i, j, k, l,temp;
+    //最多会循环n-1次
+    for (i = 0; i < 16-1; i++) {
+        for (j = 0; j < 16-1-i; j++){
+            if ( arr[j] > arr[j+1] ) {
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        printf("排了%d次\n", i);
+        for(k = 0; k < 16; k++){
+            printf("%d\n", arr[k]);
+        }
+    }
+    printf("排序后\n");
+    for(l = 0; l < 16; l++){
+        printf("%d\n", arr[l]);
+    }
+    return 0;
+}
